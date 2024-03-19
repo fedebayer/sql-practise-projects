496071c55be8902ac5f0af896dcc18a7d17d10bd
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-03-19 18:00:32 -0300

    Added script point 3

--- clients-providers-products/scripts.sql
+++ clients-providers-products/scripts.sql
@@ -13,3 +13,18 @@ INSERT INTO CondicionIva (Codigo, Descripcion) VALUES ('001AA', 'Monotributo'),
 ('005EE', 'Consumidor Final'), ('006FF', 'Exento');
 
 SELECT * FROM CondicionIva;
+
+/*PUNTO 3*/
+INSERT INTO Producto (Codigo, Descripcion, PrecioCompra, PrecioVenta, Estado) VALUES ('AAA11', 'Nvidia GT700', 75999.00, 80000.00, 1),
+('BBB22', 'AMD Radeon RX 7000', 100000.00, 110000.00, 1), ('CCC33', 'Intel Core i9-13900K', 50000.00, 55000.00, 1),
+('DDD44', 'Samsung Galaxy S23 Ultra', 20000.00, 22000.00, 1), ('EEE55', 'Apple iPhone 15', 15000.00, 16000.00, 1),
+('FFF66', 'Sony PlayStation 6', 10000.00, 11000.00, 1), ('GGG77', 'Xbox Series X/S', 5000.00, 55000.00, 1),
+('HHH88', 'Laptop ASUS ROG G15', 30000.00, 33000.00, 1), ('III99', 'Monitor Samsung Odyssey', 20000.00, 22000.00, 1),
+('JJJAA', 'Impresora HP LaserJet Pro M404dw', 10000.00, 11000.00, 1), ('KKKBB', 'Escritorio IKEA Malm', 5000.00, 55000.00, 1),
+('LLLCC', 'Sofá IKEA Kivik', 30000.00, 33000.00, 1), ('MMMDD', 'Televisión Samsung 65" 4K UHD', 20000.00, 22000.00, 1), 
+('NNNEE', 'Nevera LG InstaView Door-in-Door', 10000.00, 11000.00, 1), ('OOOFF', 'Lavadora Samsung WW90T854DBX', 5000.00, 55000.00, 1),
+('PPPGG', 'Secadora Samsung DV90T854DBX', 30000.00, 33000.00, 1), ('QQQQH', 'Microondas Whirlpool MWF425SL', 20000.00, 22000.00, 1),
+('RRRII', 'Horno eléctrico Whirlpool W7OM450S', 10000.00, 11000.00, 1), ('OOFFF', 'Lavadora', 123000.00, 140000.00, 1),
+('SSSJJ', 'Mini linterna LED', 200.00, 240.00, 1);
+
+SELECT * FROM Producto;
