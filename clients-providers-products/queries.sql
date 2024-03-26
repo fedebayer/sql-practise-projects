f8544055b9e12a5914fb7ee2b496a759414d2789
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2024-03-26 18:41:34 -0300

    Added queries objectives

---
+++ clients-providers-products/queries.sql
@@ -0,0 +1,6 @@
+PUNTOS
+1. Listar tipo de documento (descripción), número de doc., condición de IVA (descripción), apellido y nombres de los clientes
+2. Listar el CUIT y Razón Social y condición de IVA (descripción) de los proveedores que posean sean Monotributristas ordenados por CUIT
+3. Listar los productos cuyo precio de venta sea mayor a 100 y menor a 250 (ver BEETWEN)
+4. Listar los 5 productos más caros (mostrar código, descripción, precio de compra)
+5. Listar los 5 productos más baratos (mostrar código, descripción, precio de compra)
